5a67986f78a7bd765444a852b457bb936847ffaf
xinge0721 <2319830499@qq.com>
2024-10-25 14:52:20 +0800

    1

--- stepping/stepping - 副本.h
+++
@@ -1,9 +0,0 @@
-#ifndef __stepping_H
-#define __stepping_H	 
-#include "sys.h"
-void stepping_init(void);
-void stepping_PWM(uint32_t angle,uint32_t speed);
-void stepping_reversal(void);
-void stepping_End(void);
-
-#endif
